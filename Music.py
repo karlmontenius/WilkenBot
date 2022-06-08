58203f5f0678c5e026b674e27d34a1dff15566a5
karlmontenius <85098994+karlmontenius@users.noreply.github.com>
2022-06-08 17:57:33 +0200

    Added pause and resume commands

--- Music.py
+++ Music.py
@@ -1,3 +1,4 @@
+from asyncpg import connect
 import discord
 from discord.ext import commands
 import youtube_dl
@@ -6,7 +7,8 @@ import json
 from dpymenus import Page, ButtonMenu
 from asyncio import sleep
 
-
+vc = []
+queue = []
 
 class Music(commands.Cog, description="General commands, such as !slap, and !joke!"):
     def __init__(self, bot):
@@ -31,6 +33,22 @@ class Music(commands.Cog, description="General commands, such as !slap, and !jok
         parsedUrl3 = parsedJson["videos"][2]["url_suffix"]
         parsedTitle3 = parsedJson["videos"][2]["title"]
         parsedDuration3 = parsedJson["videos"][2]["duration"]
+
+        async def playmusic(self, ctx, url):
+            YDL_OPTIONS = {'format': 'bestaudio/best', 'noplaylist':'True'}
+            FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+            channel = ctx.author.voice.channel
+            global vc
+            global queue
+            if channel != None:
+                vc = await channel.connect()
+            with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(url, download=False)
+                I_URL = info['formats'][0]['url']
+                source = await discord.FFmpegOpusAudio.from_probe(I_URL, **FFMPEG_OPTIONS)
+                vc.play(source)
+                vc.is_playing()
+
         
         async def first(menu):
             if menu.button_pressed(one):
@@ -51,19 +69,6 @@ class Music(commands.Cog, description="General commands, such as !slap, and !jok
                 url = "https://youtube.com" + parsedUrl3
                 await playmusic(self, ctx, url)
 
-        async def playmusic(self, ctx, url):
-            YDL_OPTIONS = {'format': 'bestaudio/best', 'noplaylist':'True'}
-            FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-            channel = ctx.author.voice.channel
-            if channel != None:
-                vc = await channel.connect()
-            with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-                info = ydl.extract_info(url, download=False)
-                I_URL = info['formats'][0]['url']
-                source = await discord.FFmpegOpusAudio.from_probe(I_URL, **FFMPEG_OPTIONS)
-                vc.play(source)
-                vc.is_playing()
-
         page1 = Page(title='Song results | Music', description='Which song do you want to play?')
         page1.add_field(name="1 - " + parsedTitle1, value=parsedDuration1, inline=False)
         page1.add_field(name="2 - " + parsedTitle2, value=parsedDuration2, inline=False)
@@ -73,9 +78,16 @@ class Music(commands.Cog, description="General commands, such as !slap, and !jok
         menu.add_pages([page1])
         await menu.open()
 
-    @commands.command(name='Stop', brief="Searches for keyword on youtube")
-    async def stop(self, ctx):
-        discord.VoiceClient.pause
+    @commands.command(name='Pause', brief="Searches for keyword on youtube")
+    async def pause(self, ctx):
+        global vc
+        vc.pause()
+
+    @commands.command(name='Resume', brief="Searches for keyword on youtube")
+    async def resume(self, ctx):
+        global vc
+        vc.resume()
+
 
 def setup(bot):
     bot.add_cog(Music(bot))
