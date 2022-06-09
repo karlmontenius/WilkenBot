2168718aab1303f7138ff41a00a17864d338cd2f
karlmontenius <85098994+karlmontenius@users.noreply.github.com>
2022-06-09 23:34:03 +0200

    Updated to discord.py 2.0

--- WilkenBot.py
+++ WilkenBot.py
@@ -1,28 +1,31 @@
 import discord
 from discord.ext import commands
-from pretty_help import DefaultMenu, PrettyHelp
+import asyncio
+#from pretty_help import DefaultMenu, PrettyHelp
 
 #--------------DEFINITIONS-------------------------------------------------------------
-bot = commands.Bot(command_prefix="!", case_insensitive=True)
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", case_insensitive=True, intents=intents)
 bot.owner_ids = [238834157181075456]
 startup_extensions = ["Commands",
                       "Music"]
 
-menu = DefaultMenu('◀️', '▶️', '❌')
-bot.help_command = PrettyHelp(navigation=menu, color=discord.Colour.green()) 
+# menu = DefaultMenu('◀️', '▶️', '❌')
+# bot.help_command = PrettyHelp(navigation=menu, color=discord.Colour.green()) 
 #-------------------READY------------------------------------------------------------------------------------------
-if __name__ == "__main__":
-    for extension in startup_extensions:
-        try:
-            bot.load_extension(extension)
-        except Exception as e:
-            exc = '{}: {}'.format(type(e).__name__, e)
-            print('Failed to load extension {}\n{}'.format(extension, exc))
+async def loader():
+    if __name__ == "__main__":
+        for extension in startup_extensions:
+            try:
+                await bot.load_extension(extension)
+                print("loaded")
+            except Exception as e:
+                exc = '{}: {}'.format(type(e).__name__, e)
+                print('Failed to load extension {}\n{}'.format(extension, exc))
 
-@bot.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
-    await bot.change_presence(activity=discord.Game(name="Made by: MiniMonty"))
+async def main():
+    await loader()
+    async with bot:
+        await bot.start('OTY2Njc5NDcwMjU5OTY2MDky.YmFQaQ.MDXz8e7dz5-leNdpOZrZrPSEqCU')
 
-#-------START----------------------------------------------------------------------------------
-bot.run('OTY2Njc5NDcwMjU5OTY2MDky.YmFQaQ.MDXz8e7dz5-leNdpOZrZrPSEqCU')
+asyncio.run(main())
