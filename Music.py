51e6b3a463f2227e9aa4bfc6c74aa5e79cd013f8
karlmontenius <85098994+karlmontenius@users.noreply.github.com>
2022-06-12 18:51:56 +0200

    cleanup

--- Music.py
+++ Music.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 import youtube_dl
 from youtube_search import YoutubeSearch
 import json
-from dpymenus import Page, ButtonMenu
 import datetime
 import asyncio
 
@@ -17,16 +16,8 @@ class Music(commands.Cog, description="Commands that control the music from the 
         self.bot = bot
         self._last_member = None
 
-
-
-
     @commands.command(name='Play', brief="Search for the song that you want to play.")
     async def play(self, ctx, keyword):
-        one = '1️⃣'
-        two = '2️⃣'
-        three = '3️⃣'
-        stop = '❌'
-        ydl_opts = {}
         results = YoutubeSearch(f"{keyword}", max_results=3).to_json()
         parsedJson = json.loads(results)
         parsedUrl1 = parsedJson["videos"][0]["url_suffix"]
@@ -38,7 +29,6 @@ class Music(commands.Cog, description="Commands that control the music from the 
         parsedUrl3 = parsedJson["videos"][2]["url_suffix"]
         parsedTitle3 = parsedJson["videos"][2]["title"]
         parsedDuration3 = parsedJson["videos"][2]["duration"]
-
 
         async def playmusic(self, ctx, url, duration):
             YDL_OPTIONS = {'format': 'bestaudio/best', 'noplaylist':'True'}
@@ -107,38 +97,6 @@ class Music(commands.Cog, description="Commands that control the music from the 
                 await msg.delete()
 
         msg = await ctx.send(embed=embedsong ,view=Buttons())
-
-
-        # async def first(menu):
-        #     if menu.button_pressed(one):
-        #         await menu.close()
-        #         url = "https://youtube.com" + parsedUrl1
-        #         duration = parsedDuration1
-        #         await playmusic(self, ctx, url, duration)
-
-        #     if menu.button_pressed(two):
-        #         await menu.close()
-        #         url = "https://youtube.com" + parsedUrl2
-        #         duration = parsedDuration2
-        #         await playmusic(self, ctx, url, duration)
-
-        #     if menu.button_pressed(stop):
-        #         await menu.close()
-
-        #     elif menu.button_pressed(three):
-        #         await menu.close()
-        #         url = "https://youtube.com" + parsedUrl3
-        #         duration = parsedDuration3
-        #         await playmusic(self, ctx, url, duration)
-
-        # page1 = Page(title='Song results | Music', description='Which song do you want to play?')
-        # page1.add_field(name="1 - " + parsedTitle1, value=parsedDuration1, inline=False)
-        # page1.add_field(name="2 - " + parsedTitle2, value=parsedDuration2, inline=False)
-        # page1.add_field(name="3 - " + parsedTitle3, value=parsedDuration3, inline=False)
-        # page1.buttons([one, two, three, stop]).on_next(first)
-        # menu = ButtonMenu(ctx)
-        # menu.add_pages([page1])
-        # await menu.open()
 
         async def waitforsong(self, ctx, duration):
             durationsecs = 2
